0026cfd3ba067a37de712f65b032b50b3a06f550
James O'Neill <james@jamesoneill.eu>
2017-09-14 23:07:07 +0100

    Create buttons for new publication actions

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace App\Providers;
 
-use Laravel\Dusk\DuskServiceProvider;
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -14,7 +14,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Blade::if('published', function ($post) {
+            return $post->is_published;
+        });
     }
 
     /**
