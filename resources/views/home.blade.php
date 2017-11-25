8e74c0a3de6ba9f6fcf83ecd7edcdcc07e5cb7d1
James O'Neill <james@jamesoneill.eu>
2017-11-25 16:22:07 +0000

    Move home page content back to its own file

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,35 +1,32 @@
 @extends('layouts.master')
 
 @section('content')
-    <p class="text-grey-darkest my-3 text-sm">I'm a software developer from Bristol in the South West of England.</p>
+    <div class="container mx-auto">
+        <h1 class="text-5xl leading-none my-4 text-shadow-1">
+            Hi, I'm James O'Neill.<br>
+            I'm a software developer.
+        </h1>
 
-    <p class="text-grey-darkest my-3 text-sm">
-        Day to day I mostly work with Laravel and React. This site is your
-        source of information for whatever I happen to be up to, be it related
-        to software or any of my other interests.
-    </p>
+        <div class="w-2/3">
+            <p class="py-4 leading-normal text-blue-dark">
+                I mainly work on the web and specialise in Laravel and
+                React but I’m interested in all aspects of modern
+                software development and always interested in improving
+                my skills.
+            </p>
 
-    <p class="text-grey-darkest my-3 text-sm">
-        If you want to say hi why not follow me on
-        <a class="text-red no-underline" href="https://twitter.com/jamesoneill83">Twitter</a> or you can drop
-        me an email at <a class="text-red no-underline" href="mailto:james@jamesoneill.eu">james@jamesoneill.eu</a>.
-        Also you could <a class="text-red no-underline" href="https://www.facebook.com/ambertibetanterrier/">like my Dog on Facebook.</a>
-    </p>
+            <p class="py-4 leading-normal text-blue-dark">
+                Write a paragraph about something else.
+            </p>
 
-    <div class="my-8">
-        @if($posts->count())
-            <ul class="list-reset mx-0 my-3">
-                @foreach($posts as $post)
-                    <li class="list-reset my-3">
-                        <div class="my-0 text-sm text-grey-dark">{{ $post->published_at->format('jS F Y') }}</div>
-                        <h2 class="my-0 text-red">
-                            <a class="no-underline text-black" href="{{ $post->url() }}">{{ $post->title }}</a>
-                        </h2>
-                    </li>
-                @endforeach
-            </ul>
-        @endif
+            <p class="py-4 leading-normal text-blue-dark">
+                If you like what you’ve read on my site and want to chat
+                then I’d love to hear from you. You could drop me an
+                email at james@jamesoneill.eu or send a tweet to
+                <a href="https://twitter.com/jamesoneill83">@jamesoneill83</a>.
+                If you’re in Bristol and want to talk then it would be
+                great to grab coffee some time.
+            </p>
+        </div>
     </div>
-
-    <mailing-list-signup />
 @endsection
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -62,34 +62,7 @@
                 </header>
             @show
 
-            <div class="container mx-auto">
-                <h1 class="text-5xl leading-none my-4 text-shadow-1">
-                    Hi, I'm James O'Neill.<br>
-                    I'm a software developer.
-                </h1>
-
-                <div class="w-2/3">
-                    <p class="py-4 leading-normal text-blue-dark">
-                        I mainly work on the web and specialise in Laravel and
-                        React but I’m interested in all aspects of modern
-                        software development and always interested in improving
-                        my skills.
-                    </p>
-
-                    <p class="py-4 leading-normal text-blue-dark">
-                        Write a paragraph about something else.
-                    </p>
-
-                    <p class="py-4 leading-normal text-blue-dark">
-                        If you like what you’ve read on my site and want to chat
-                        then I’d love to hear from you. You could drop me an
-                        email at james@jamesoneill.eu or send a tweet to
-                        <a href="https://twitter.com/jamesoneill83">@jamesoneill83</a>.
-                        If you’re in Bristol and want to talk then it would be
-                        great to grab coffee some time.
-                    </p>
-                </div>
-            </div>
+            @yield('content')
 
             @section('footer')
                 <footer class="container mx-auto">
