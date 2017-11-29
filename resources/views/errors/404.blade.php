4f56af96deefe9c7e025c4fb04e905a977598e31
James O'Neill <james@jamesoneill.eu>
2017-11-29 22:08:50 +0000

    Tidy 404 page for mobile

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,13 +1,23 @@
 @extends('layouts.master')
 
 @section('content')
-    <h1 class="text-center mb4">THANK YOU MARIO! BUT OUR PRINCESS IS IN ANOTHER CASTLE</h1>
+    <div class="container mx-auto px-4 md:px-0 lg:px-0">
+        <h1 class="text-3xl md:text-5xl lg:text-5xl leading-none my-4 text-grey-darkest">
+            Thank You Mario<br>
+            But Our Princess Is in Another Castle
+        </h1>
 
-    <p>
-        I'm sorry but it looks like you've come to the wrong page. Then again
-        maybe not, maybe all of the pages are "wrong" except this one. Who am I
-        to judge?
-    </p>
+        <div class="md:w-2/3">
+            <p class="py-4 leading-normal text-grey-darker">
+                I'm sorry but it looks like you've come to the wrong page. Then
+                again maybe not, maybe all of the pages are "wrong" except
+                this one. Who am I to judge?
+            </p>
 
-    <p>But in all seriousness, this is my 404 page. You know what to do.</p>
+            <p class="py-4 leading-normal text-grey-darker">
+                But in all seriousness, this is my
+                404 page. You know what to do.
+            </p>
+        </div>
+    </div>
 @endsection
