895b3e419538e80db0d2b9cc1a9abb9eb4ec4099
James O'Neill <james@jamesoneill.eu>
2017-11-29 21:11:12 +0000

    Adjust blog styling for mobile

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -1,17 +1,20 @@
 @extends('layouts.master')
 
 @section('content')
-    <div class="container mx-auto">
+    <div class="container mx-auto px-4 md:px-0 lg:px-0">
         <h1 class="text-3xl leading-none my-4">Articles I've Written</h1>
 
-        <ul class="pl-0 w-3/4">
+        <ul class="pl-0 md:w-3/4 lg:w-3/4">
             @foreach($posts as $post)
                 <li class="list-reset mb-4">
                     <a href="{{ $post->url() }}" class="block mb-1 text-xl">
                         {{ $post->title }}
                     </a>
 
-                    <div class="text-sm text-grey-darker mb-3">{{ $post->published_at->format('jS F Y') }}</div>
+                    <div class="text-sm text-grey-darker mb-3">
+                        {{ $post->published_at->format('jS F Y') }}
+                    </div>
+
                     <p>{{ $post->description }}</p>
                 </li>
             @endforeach
