868eabfd4bec4e843203be5f5c882c495dd1153a
James O'Neill <james@jamesoneill.eu>
2017-12-01 07:45:19 +0000

    Truncate blog excerpt to a single line

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -15,7 +15,7 @@
                         {{ $post->published_at->format('jS F Y') }}
                     </div>
 
-                    <p>{{ $post->description }}</p>
+                    <p class="truncate">{{ $post->description }}</p>
                 </li>
             @endforeach
         </ul>
