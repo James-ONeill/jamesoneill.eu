e8ac0143eab8a500adee27306cbf75a02406fb53
James O'Neill <james@jamesoneill.eu>
2018-05-20 12:40:10 +0100

    Begin update of main dashboard layout

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -6,27 +6,8 @@
 
 @section('main')
     <div class="flex mb-8">
-        @section('sidebar')
-            <nav class="w-64 border-r mr-8 px-8">
-                <ul class="list-reset">
-                    <li>
-                        <a class="text-blue text-lg font-bold" href="{{ route('dashboard.posts.index') }}">
-                            Posts
-                        </a>
-                    </li>
-                    <li>
-                        <a class="text-blue text-lg font-bold" href="{{ route('dashboard.talks.index') }}">
-                            Talks
-                        </a>
-                    </li>
-                </ul>
-            </nav>
-        @show
-
         <div class="flex-grow max-w-xl mx-auto">
             @yield('content')
         </div>
-
-        <div class="border-l border-transparent ml-8 px-8 w-64"></div>
     </div>
 @endsection
